8c72c06f86cd9549b6fa10f6c94d866148f34695
Anjan Roy <anjanroy@yandex.com>
2020-01-12 14:48:55 +0530

    added concurrency support in solution of problem 12; runs a lot faster now :)

--- projecteuler/problem12.go
+++ projecteuler/problem12.go
@@ -1,11 +1,16 @@
 package projecteuler
 
-// GetXthTriangularNumber - ...
+import (
+	"fmt"
+	"sync"
+)
+
+// GetXthTriangularNumber - Returns triangular number at given position `x`
 func GetXthTriangularNumber(x int) int {
 	return x * (x + 1) / 2
 }
 
-// GetFactorCount - ...
+// GetFactorCount - Return number of factors a given number have
 func GetFactorCount(x int) int {
 	if x == 1 {
 		return 1
@@ -19,17 +24,68 @@ func GetFactorCount(x int) int {
 	return count
 }
 
+// TriangularNumber - Holds a triangular number, along with number of its factors
+type TriangularNumber struct {
+	num     int
+	factorC int
+}
+
+func getHighlyDivisibleTriNumFromRange(init int, end int, maxFactorC int, channel chan TriangularNumber, channelEnd chan bool) {
+	triNum := TriangularNumber{1, 1}
+	for i := init; i <= end; i++ {
+		select {
+		case <-channelEnd:
+			return
+		default:
+			tmpX := GetXthTriangularNumber(i)
+			if tmp := GetFactorCount(tmpX); tmp > triNum.factorC {
+				triNum.factorC = tmp
+				triNum.num = tmpX
+				if triNum.factorC >= maxFactorC {
+					break
+				}
+			}
+		}
+	}
+	channel <- triNum
+	if triNum.factorC >= maxFactorC {
+		close(channel)
+	}
+}
+
 // HighlyDivisibleTriangularNumber - ...
 func HighlyDivisibleTriangularNumber() int {
-	triNum := 1
-	factorC := 1
-	i := 1
-	for factorC <= 500 {
-		triNum = GetXthTriangularNumber(i)
-		if tmp := GetFactorCount(triNum); tmp > factorC {
-			factorC = tmp
+	triNum := TriangularNumber{1, 1}
+	channel := make(chan TriangularNumber, 2)
+	channelEnd := make(chan bool)
+	startAt := 1
+	workerC := 4
+	createWorkers := func(startAt *int, incrBy int, c int) {
+		for i := 0; i < c; i++ {
+			go getHighlyDivisibleTriNumFromRange(*startAt, *startAt+incrBy-1, 500, channel, channelEnd)
+			*startAt += incrBy
 		}
-		i++
 	}
-	return triNum
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		respC := 1
+		for v := range channel {
+			if v.factorC > triNum.factorC {
+				triNum = v
+			}
+			fmt.Println(v)
+			if triNum.factorC >= 500 {
+				channelEnd <- true
+				wg.Done()
+			}
+			respC++
+			if respC%workerC == 0 {
+				createWorkers(&startAt, 250, workerC)
+			}
+		}
+	}()
+	createWorkers(&startAt, 500, workerC)
+	wg.Wait()
+	return triNum.num
 }
