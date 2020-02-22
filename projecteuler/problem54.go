23db3171d2769e02c1018aed61e421ee34a5af37
Anjan Roy <anjanroy@yandex.com>
2020-02-22 21:08:01 +0530

    completed code commenting for problem 54;

--- projecteuler/problem54.go
+++ projecteuler/problem54.go
@@ -13,14 +13,17 @@ type hand struct {
 }
 
 // given a hand of five cards, it'll get us
-// a mapping of card value to card suits
-// if we've a multiple cards of same value which
+// a mapping from card value to card suits
+// if we've multiple cards of same value which
 // belong to different suits, then we'll keep them
 // in a container, for that certain card value
 //
 // e.g. we've a hand of cards [5C 5D 9C AD AC]
 // which can be represented as a map {3:[C D] 7:[C] 12:[D C]}
 // where 3, 7, 12 i.e. keys are numeric representation of card values, see more about it below
+//
+// now I've incorporated way to map only a subset of cards in hand,
+// where subset is no doubt denoted by its starting and ending indices ( inclusive )
 func (h hand) getMap(from int, to int) map[int][]string {
 	buffer := make(map[int][]string)
 	for i := from; i <= to; i++ {
@@ -35,6 +38,8 @@ func (h hand) getMap(from int, to int) map[int][]string {
 	return buffer
 }
 
+// given a hand of cards, it'll sort them in ascending fashion
+// as per card values i.e. 2 < 3 < 4 < 5 < 6 < 7 < 8 < 9 < 10 < Jack < Queen < King < Ace
 func (h *hand) sort() {
 	for i := 0; i < len(h.cards); i++ {
 		for j := 0; j < len(h.cards)-i-1; j++ {
@@ -45,6 +50,22 @@ func (h *hand) sort() {
 	}
 }
 
+/*
+	From here on we'll start writing logic for
+	determining whether a given hand ( combination
+	of cards sorted in ascending fashion ) satisfies a property or not,
+	using which we can determine, winner of current hand
+
+	Well following 10 functions will help us in ranking
+	a hand, which are presented in their ascending order
+	of importance
+*/
+
+// finds highest card value for a subset of cards
+// in hand now
+//
+// card values to be expressed in their corresponding numeric values
+// starting from 1 to 13
 func (h hand) getHighCard(from int, to int) int {
 	max := 0
 	for k := range h.getMap(from, to) {
@@ -55,6 +76,9 @@ func (h hand) getHighCard(from int, to int) int {
 	return max
 }
 
+// checks whether we've a pair of cards of same value
+// in case of success returns card value, with which we've a pair of cards
+// else returns 0, to denote abscence
 func (h hand) getOnePair() int {
 	max := 0
 	for k, v := range h.getMap(0, 4) {
@@ -65,6 +89,10 @@ func (h hand) getOnePair() int {
 	return max
 }
 
+// checks whether we've two different pairs
+// where each with same card value
+// in case of success returns a slice of those card values
+// in case of abscence returns `nil`
 func (h hand) getTwoPairs() []int {
 	pairs := []int{}
 	for k, v := range h.getMap(0, 4) {
@@ -73,11 +101,13 @@ func (h hand) getTwoPairs() []int {
 		}
 	}
 	if len(pairs) == 2 {
-		return bubbleSort(pairs)
+		return pairs
 	}
 	return nil
 }
 
+// finds card value for which we've three cards
+// else returns 0, to denote abscence
 func (h hand) getThreeOfAKind() int {
 	max := 0
 	for k, v := range h.getMap(0, 4) {
@@ -88,6 +118,8 @@ func (h hand) getThreeOfAKind() int {
 	return max
 }
 
+// checks whether all cards in hand
+// are of consequtive value or not
 func (h hand) isStraight() bool {
 	check := true
 	cur := cardValueToIndex(h.cards[0][:1])
@@ -102,6 +134,9 @@ func (h hand) isStraight() bool {
 	return check
 }
 
+// checks whether all cards in hand are of same suit
+// or not i.e. need to check second value of each card in hand
+// e.g. for 5H, 9C, 6H , we'll check {H, C, H}
 func (h hand) isFlush() bool {
 	check := true
 	cur := ""
@@ -120,10 +155,15 @@ func (h hand) isFlush() bool {
 	return check
 }
 
+// checks whether we've three cards of same kind
+// and a pair of cards with same value, returns corresponding
+// values respectively
 func (h hand) getFullHouse() (int, int) {
 	return h.getThreeOfAKind(), h.getOnePair()
 }
 
+// finds card value, with which we've four cards
+// else returns 0
 func (h hand) getFourOfAKind() int {
 	max := 0
 	for k, v := range h.getMap(0, 4) {
@@ -134,10 +174,20 @@ func (h hand) getFourOfAKind() int {
 	return max
 }
 
+// checks whether all cards are of consecutive values
+// and of same suit
+//
+// whether they belong to same suit can be checked using
+// isFlush() function, defined above
 func (h hand) isStraightFlush() bool {
 	return h.isStraight() && h.isFlush()
 }
 
+// chceks whether we've 10, Jack, Queen, King, Ace
+// and all are of same suit
+//
+// whether they belong to same suit can be checked using
+// isFlush() function, defined above
 func (h hand) isRoyalFlush() bool {
 	count := 0
 	for _, v := range h.cards {
@@ -149,6 +199,16 @@ func (h hand) isRoyalFlush() bool {
 	return count == 5 && h.isFlush()
 }
 
+// given a hand of cards, we'll compute its rank
+// depending upon above defined 10 functions
+// so it's pretty evident we'll have 10 ranks
+// starting from 1 and running upto 10
+//
+// we'll it may be the situation, when hand
+// satisfies rank 3 & 2 criteria both,
+// then we'll choose higher rank, cause these
+// ranks will help us in deciding winner
+// between two players
 func (h hand) getRank() int {
 	if h.isRoyalFlush() {
 		return 10
@@ -180,11 +240,21 @@ func (h hand) getRank() int {
 	return 1
 }
 
+// given a card value, returns corresponding numeric
+// representation, which will be really helpful in comparing
+// two cards or while sorting cards ascendically
+// i.e. maps {2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Queen, King, Ace}
+// to {1, 2, ..., 12, 13}
 func cardValueToIndex(a string) int {
 	cardVToIdxMap := map[string]int{"2": 1, "3": 2, "4": 3, "5": 4, "6": 5, "7": 6, "8": 7, "9": 8, "T": 9, "J": 10, "Q": 11, "K": 12, "A": 13}
 	return cardVToIdxMap[a]
 }
 
+// given two card values, determines whether we need
+// to swap them in order to obtain a ascendically
+// sorted card sequence or not
+//
+// corresponding numeric values of cards to be used for comparison
 func needsSwap(a string, b string) bool {
 	if aIdx, bIdx := cardValueToIndex(a), cardValueToIndex(b); aIdx < bIdx {
 		return false
@@ -194,6 +264,10 @@ func needsSwap(a string, b string) bool {
 	return true
 }
 
+// splits hand to two players into two different hands
+// where first one is for player one & second one for
+// player two.
+// i.e. processes each line of input data file
 func getHandsOfTwoPlayers(hands string) (hand, hand) {
 	tmp := strings.Split(hands, " ")
 	return hand{tmp[:5]}, hand{tmp[5:]}
@@ -210,17 +284,14 @@ func getAllHands(filepath string) []string {
 	return strings.Split(string(content), "\n")
 }
 
-func bubbleSort(arr []int) []int {
-	for i := 0; i < len(arr); i++ {
-		for j := 0; j < len(arr)-i-1; j++ {
-			if arr[j] > arr[j+1] {
-				swapNum(&arr[j], &arr[j+1])
-			}
-		}
-	}
-	return arr
-}
-
+// determines winner between two hands,
+// using clause 1 i.e. high card value
+// we'll there might be a situation, when both
+// hands having same high card i.e. `Q`,
+// then we'll require to compare next high card value,
+// if there's also a tie, we'll go for next high card value
+// in this manner, we'll keep going on, until we obtain a solution
+// with out a tie or we exhaust availble cards, whichever happens first
 func determineWinnerwrtHighCard(h1 hand, h2 hand) int {
 	hCard1, hCard2 := 0, 0
 	to := 4
@@ -236,6 +307,16 @@ func determineWinnerwrtHighCard(h1 hand, h2 hand) int {
 	return 2
 }
 
+// given two hands, which are having a tie
+// at a given `rank`, we'll try to resolute conflict;
+// at the very end, we'll resort to finding high card value
+// between those two hands
+//
+// as per given description in question, there's always
+// a clear winner, so I'm keeping this in mind
+// but in case of previous function, where I tried finding winner
+// using high card value, I've kept one provision, which returns
+// -1, to denote it was unable to find any clear winner
 func tieBreaker(h1 hand, h2 hand, rank int) int {
 	winner := 0
 	switch rank {
@@ -295,6 +376,12 @@ func tieBreaker(h1 hand, h2 hand, rank int) int {
 	return winner
 }
 
+// given a pair of hands ( of two competing players )
+// first we'll sort them ascendically using card
+// values ( numeric equivalent )
+// then we'll try to compute rank for each of two hands,
+// if they tie, we'll go for tie breaking, else we'll simply
+// return winner ( as per who's having a larger rank )
 func getWinner(h1 hand, h2 hand) int {
 	h1.sort()
 	h2.sort()
@@ -306,7 +393,7 @@ func getWinner(h1 hand, h2 hand) int {
 	return 2
 }
 
-// PokerHands - ...
+// PokerHands - Computes how many times player one wins
 func PokerHands() int {
 	count := 0
 	hands := getAllHands("./p054_poker.txt")
